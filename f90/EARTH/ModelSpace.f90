729eca85b11e68d4bafcdf651a52d42d6dbfcd69
Anna Kelbert <anna.kelbert@cfa.harvard.edu>
2010-10-29 19:00:44 +0000

    Failed to add ModelSpace to the global code... fixed.

---
+++ f90/EARTH/ModelSpace.f90
@@ -0,0 +1,15 @@
+! *****************************************************************************
+module ModelSpace
+    ! This module contains the type definitions for the structures that contain
+    ! the full information about the model parametrization
+
+  use model_operators
+  use modeldef
+  use modelmap
+  implicit none
+
+!Contains
+
+  ! Empty module that gathers together the rest of the model parameter modules
+
+end module ModelSpace
