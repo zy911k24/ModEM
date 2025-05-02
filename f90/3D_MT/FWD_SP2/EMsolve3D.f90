76b4f2a4243f92b564ab8b4693cf097d333c5a8f
Miles Curry <2590700+MiCurry@users.noreply.github.com>
2025-05-02 09:15:30 -0600

    Revert "Do not use FWDSolve3Dp if FG is not defined"

--- f90/3D_MT/FWD_SP2/EMsolve3D.f90
+++ f90/3D_MT/FWD_SP2/EMsolve3D.f90
@@ -21,9 +21,7 @@ module EMsolve3D
 
   interface FWDSolve3D
      MODULE PROCEDURE FWDSolve3Ds
-#ifdef FP
      MODULE PROCEDURE FWDSolve3Dp
-#endif
   end interface
 
   type :: emsolve_control
@@ -571,7 +569,6 @@ Contains
 
   end subroutine FWDsolve3Ds
 
-#ifdef FG
   ! fine-grained parallel version
   subroutine FWDsolve3Dp(bRHS,omega,eSol,device_id,comm_local)
 !----------------------------------------------------------------------
@@ -1206,7 +1203,6 @@ Contains
      deallocate(KSSiter%rerr)
 
   end subroutine FWDsolve3Dp
-#endif
 
 !**********************************************************************
 ! solver_divcorrr contains the subroutine that would solve the divergence
